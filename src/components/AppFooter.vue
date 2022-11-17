a18c80deb55f7373ac6a8eaefdd530e1c3ad93c6
V1tale <elativ.c@gmail.com>
2022-11-17 16:11:26 +0100

    completata struttura footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,6 +11,27 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+  <section id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </section>
+</template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#footer {
+  height: 250px;
+  background-color: rebeccapurple;
+  .col-3 {
+    height: 200px;
+    background-color: orangered;
+    border:2px solid yellow;
+  }
+}
+</style>
