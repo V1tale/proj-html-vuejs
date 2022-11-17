cb29486b1eb865554282df4a1730bf74833fa7cd
V1tale <elativ.c@gmail.com>
2022-11-17 15:35:53 +0100

    completata struttura home

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,6 +11,48 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+  <!-- APP HOME -->
+  <section id="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-6 left"></div>
+        <div class="col-6 right"></div>
+      </div>
+      <div class="benefit row">
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+      </div>
+      <div class=" tricks row">
+        <div class="col-6"></div>
+        <div class="col-6"></div>
+      </div>
+    </div>
 
-<style lang="scss"></style>
+  </section>
+  <!-- /APP HOME -->
+</template>
+
+<style lang="scss">
+#home {
+  height: 200px;
+  & .left {
+
+    height: 200px;
+    background-color: lime;
+  }
+  & .right {
+    height:200px;
+    background-color: fuchsia;
+  }
+  .col-4 {
+    height: 200px;
+    border: 2px solid black;
+  }
+  .tricks .col-6 {
+    height: 250px; 
+    border: 2px solid green;
+  }
+}
+</style>
